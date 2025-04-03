b0493f7237a9f340e81bf3f623808135608d0cac
sunsigu <167049010+aguirremac17@users.noreply.github.com>
2025-04-03 13:12:02 +0800

    chore: add listener and update badge inquiry count

--- middleware/04.global-socket.global.ts
+++ middleware/04.global-socket.global.ts
@@ -1,10 +1,12 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const { computeBadgeCount } = useNotification();
     const { loggedIn, currentUser, } = useLocalAuth();
-    const { connect} = useGlobalSocket();
+    const { connect, removeAllListener} = useGlobalSocket();
 
    if(loggedIn.value && import.meta.client){
     await connect()
+   } else {
+    removeAllListener()
    }
 
 })
