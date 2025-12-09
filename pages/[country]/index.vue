a4d9fe53b9b63b28fef1f15e3cdf9eb931a2277a
sstteevveennnn00017 <steven00017aquino@gmail.com>
2025-12-09 22:06:33 +0800

    Front-End-Updated

--- pages/[country]/index.vue
+++ pages/[country]/index.vue
@@ -27,15 +27,15 @@
                   <v-form ref="formValid">
                     <v-card color="white" class="rounded-lg pa-5" max-width="100%">
                       <v-row class="pa-2" justify="center">
+                        
                         <v-col cols="12" sm="6" md="3" lg="3" xl="3" class="pb-0">
                           <v-row no-gutters>
                             <v-col cols="11">
                               <p class="text-14px font-600">WHERE</p>
                               <v-autocomplete density="compact" persistent-placeholder placeholder="Anywhere"
-                                variant="plain" menu-icon="mdi-map-marker-outline" :items="municipalities" v-model="location"
-                                :rules="locationRules" required item-title="name" item-value="name"
-                                 @keydown.enter="onSubmit"
-                                @click:append-inner="" :active="true"/>
+                                variant="plain" menu-icon="mdi-map-marker-outline" :items="municipalities"
+                                v-model="location" :rules="locationRules" required item-title="name" item-value="name"
+                                @keydown.enter="onSubmit" @click:append-inner="" :active="true" />
                             </v-col>
                             <v-col class="py-1 pb-5 d-flex justify-center align-end" v-if="mdAndUp">
                               <v-divider vertical thickness="2"></v-divider>
@@ -43,6 +43,7 @@
                           </v-row>
                         </v-col>
                         <v-divider v-if="xs"></v-divider>
+
                         <v-col cols="12" sm="6" md="2" lg="2" xl="2" class="pb-0">
                           <v-row no-gutters>
                             <v-col cols="11">
@@ -53,8 +54,8 @@
                                   <template #activator="{ props }">
                                     <v-text-field v-model="formattedCheckInDate" readonly persistent-hint v-bind="props"
                                       density="compact" persistent-placeholder placeholder="DD/MM/YYYY" variant="plain"
-                                      append-icon="mdi-calendar" @keydown.enter="onSubmit" @click:append-inner="checkInDateMenu = true"
-                                      style="flex: 1;">
+                                      append-icon="mdi-calendar" @keydown.enter="onSubmit"
+                                      @click:append-inner="checkInDateMenu = true" style="flex: 1;">
                                     </v-text-field>
                                   </template>
                                   <v-date-picker v-model="checkInDate" @input="checkInDateMenu = false" no-title
@@ -64,8 +65,9 @@
                                     </template>
                                   </v-date-picker>
                                 </v-menu>
+                                
                                 <v-select density="compact" variant="plain" :items="timeOptions" v-model="checkInTime"
-                                  persistent-placeholder placeholder="Time" style="flex: 0.8; max-width: 80px;">
+                                  persistent-placeholder placeholder="Time" style="flex: 0.8; max-width: 120px; min-width: 100px;">
                                 </v-select>
                               </div>
                             </v-col>
@@ -75,6 +77,7 @@
                           </v-row>
                         </v-col>
                         <v-divider v-if="xs"></v-divider>
+
                         <v-col cols="12" sm="6" md="2" lg="2" xl="2" class="pb-0">
                           <v-row no-gutters>
                             <v-col cols="11">
@@ -83,21 +86,23 @@
                                 <v-menu v-model="checkOutDateMenu" :close-on-content-click="false" :nudge-right="40"
                                   transition="scale-transition" offset-y min-width="290px">
                                   <template #activator="{ props }">
-                                    <v-text-field v-model="formattedCheckOutDate" readonly persistent-hint v-bind="props"
-                                      density="compact" persistent-placeholder placeholder="DD/MM/YYYY" variant="plain"
-                                      append-icon="mdi-calendar" @keydown.enter="onSubmit" @click:append-inner="checkOutDateMenu = true"
-                                      style="flex: 1;">
+                                    <v-text-field v-model="formattedCheckOutDate" readonly persistent-hint
+                                      v-bind="props" density="compact" persistent-placeholder placeholder="DD/MM/YYYY"
+                                      variant="plain" append-icon="mdi-calendar" @keydown.enter="onSubmit"
+                                      @click:append-inner="checkOutDateMenu = true" style="flex: 1;">
                                     </v-text-field>
                                   </template>
                                   <v-date-picker v-model="checkOutDate" @input="checkOutDateMenu = false" no-title
-                                    show-adjacent-months scrollable :min="checkInDate || new Date().toISOString().substring(0, 10)">
+                                    show-adjacent-months scrollable
+                                    :min="checkInDate || new Date().toISOString().substring(0, 10)">
                                     <template #actions>
                                       <v-btn color="primary" @click="checkOutDateMenu = false">OK</v-btn>
                                     </template>
                                   </v-date-picker>
                                 </v-menu>
+
                                 <v-select density="compact" variant="plain" :items="timeOptions" v-model="checkOutTime"
-                                  persistent-placeholder placeholder="Time" style="flex: 0.8; max-width: 80px;">
+                                  persistent-placeholder placeholder="Time" style="flex: 0.8; max-width: 120px; min-width: 100px;">
                                 </v-select>
                               </div>
                             </v-col>
@@ -107,13 +112,105 @@
                           </v-row>
                         </v-col>
                         <v-divider v-if="xs"></v-divider>
+
                         <v-col cols="12" sm="6" md="2" lg="2" xl="2" class="pb-0">
                           <v-row no-gutters>
                             <v-col cols="11">
                               <p class="text-14px font-600">GUESTS & WEDDING</p>
-                              <v-autocomplete density="compact" persistent-placeholder placeholder="Select type..."
-                                variant="plain" :items="guestAndWeddingOptions" v-model="selectedGuestOption"
-                                @keydown.enter="onSubmit"/>
+                              
+                              <v-menu 
+                                v-model="guestMenu" 
+                                :close-on-content-click="false" 
+                                offset-y 
+                                min-width="380"
+                                max-height="500"
+                              >
+                                <template v-slot:activator="{ props }">
+                                  <v-text-field
+                                    v-bind="props"
+                                    v-model="displayTotalSummary"
+                                    readonly
+                                    density="compact" 
+                                    variant="plain" 
+                                    placeholder="Add guests..."
+                                    append-icon="mdi-chevron-down"
+                                    @click:append-inner="guestMenu = !guestMenu"
+                                    @keydown.enter="onSubmit"
+                                  ></v-text-field>
+                                </template>
+
+                                <v-card class="rounded-lg">
+                                  <v-list density="compact" class="overflow-y-auto" style="max-height: 400px;">
+                                    
+                                    <template v-for="(item, index) in guestList" :key="index">
+                                      
+                                      <v-list-subheader 
+                                        v-if="item.type === 'header'"
+                                        class="text-high-emphasis font-weight-bold pt-2 text-uppercase text-caption bg-grey-lighten-5"
+                                        style="color: #2193b0; letter-spacing: 1px; position: sticky; top: 0; z-index: 1;"
+                                      >
+                                        {{ item.title }}
+                                      </v-list-subheader>
+
+                                      <v-list-item 
+                                        v-else
+                                        class="px-4 py-3"
+                                        :ripple="false"
+                                      >
+                                        <div class="d-flex align-center justify-space-between w-100">
+                                          <div class="text-body-2 mr-4 text-high-emphasis font-weight-medium" style="flex: 1; white-space: normal; line-height: 1.3;">
+                                            {{ item.title }}
+                                          </div>
+
+                                          <div class="d-flex align-center flex-shrink-0">
+                                            <v-btn
+                                              icon="mdi-minus"
+                                              size="x-small"
+                                              variant="outlined"
+                                              color="grey"
+                                              class="rounded-circle"
+                                              @click.stop="decrementItem(item)"
+                                              :disabled="item.count <= 0"
+                                              elevation="0"
+                                              style="width: 32px; height: 32px;"
+                                            ></v-btn>
+                                            
+                                            <span class="mx-3 font-weight-bold text-body-1 text-high-emphasis" style="min-width: 24px; text-align: center;">
+                                              {{ item.count }}
+                                            </span>
+
+                                            <v-btn
+                                              icon="mdi-plus"
+                                              size="x-small"
+                                              variant="outlined"
+                                              color="primary"
+                                              class="rounded-circle"
+                                              @click.stop="incrementItem(item)"
+                                              elevation="0"
+                                              style="width: 32px; height: 32px;"
+                                            ></v-btn>
+                                          </div>
+                                        </div>
+                                      </v-list-item>
+
+                                      <v-divider v-if="item.type === 'item' && index < guestList.length - 1 && guestList[index+1].type === 'header'"></v-divider>
+
+                                    </template>
+                                  </v-list>
+                                  
+                                  <v-divider></v-divider>
+                                  <v-card-actions class="pa-3 bg-grey-lighten-5">
+                                    <span class="text-caption text-medium-emphasis ml-2">
+                                      Total: <b>{{ totalGuestCount }}</b> Guests
+                                    </span>
+                                    <v-spacer></v-spacer>
+                                    <v-btn color="primary" variant="text" size="small" @click="resetCounts">Clear</v-btn>
+                                    <v-btn color="primary" variant="flat" size="small" @click="guestMenu = false">Done</v-btn>
+                                  </v-card-actions>
+
+                                </v-card>
+                              </v-menu>
+
                             </v-col>
                             <v-col class="py-1 pb-5 d-flex justify-center align-end" v-if="!xs">
                               <v-divider vertical thickness="2"></v-divider>
@@ -170,53 +267,101 @@ const municipalities = ref(philippineMunicipalities);
 // Check In / Check Out date and time
 const checkInDate = ref(null);
 const checkOutDate = ref(null);
-const checkInTime = ref("10:00");
-const checkOutTime = ref("11:00");
 const checkInDateMenu = ref(false);
 const checkOutDateMenu = ref(false);
 
-// Time options (24-hour format with 30-minute intervals)
+const checkInTime = ref("10:00 AM");
+const checkOutTime = ref("11:00 AM");
+
+// Time options generator (AM/PM format)
 const timeOptions = Array.from({ length: 48 }, (_, i) => {
-  const hours = Math.floor(i / 2);
-  const minutes = (i % 2) * 30;
-  return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}`;
+  const totalMinutes = i * 30;
+  const hour24 = Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60;
+  
+  const ampm = hour24 >= 12 ? 'PM' : 'AM';
+  const hour12 = hour24 % 12 || 12; 
+  
+  return `${hour12}:${String(minutes).padStart(2, "0")} ${ampm}`;
 });
 
-// Wedding-related options
-const weddingOptions = [
-  "Wedding",
-  "Asian Wedding",
-  "Outdoor Wedding",
-  "Unusual Wedding",
-  "Wedding Ceremony",
-  "Wedding Reception",
-  "Civil Ceremony",
-  "Civil Partnership",
-  "Marquee Wedding",
-  "Unique Wedding",
-  "Garden Wedding",
-  "Dry Hire Wedding",
-];
+// --- GUEST CALCULATOR LOGIC ---
 
-// Guest count options (1-500+)
-const guestCountOptions = Array.from({ length: 100 }, (_, i) => `${(i + 1) * 5} Guests`);
-guestCountOptions.unshift("1-5 Guests", "5-10 Guests", "10-25 Guests", "25-50 Guests", "50-100 Guests", "100-250 Guests", "250-500 Guests", "500+ Guests");
+interface GuestItem {
+  title: string;
+  type: 'header' | 'item';
+  count?: number; 
+}
 
-// Combine wedding and guest options
-const guestAndWeddingOptions = computed(() => {
-  const combined = [...weddingOptions, ...guestCountOptions];
-  return combined.sort();
+const guestMenu = ref(false);
+
+const guestList = ref<GuestItem[]>([
+  // 1. By Relationship - Immediate Family
+  { title: 'Immediate Family', type: 'header' },
+  { title: 'Parents of the Bride', type: 'item', count: 0 },
+  { title: 'Parents of the Groom', type: 'item', count: 0 },
+  { title: 'Siblings & In-Laws', type: 'item', count: 0 },
+  { title: 'Grandparents', type: 'item', count: 0 },
+  { title: 'Children of the Couple', type: 'item', count: 0 },
+
+  // Extended Family
+  { title: 'Extended Family', type: 'header' },
+  { title: 'Aunts & Uncles', type: 'item', count: 0 },
+  { title: 'Cousins', type: 'item', count: 0 },
+  { title: 'Nieces & Nephews', type: 'item', count: 0 },
+
+  // Social Circles
+  { title: 'Social Circles', type: 'header' },
+  { title: 'Childhood Friends', type: 'item', count: 0 },
+  { title: 'High School / College Friends', type: 'item', count: 0 },
+  { title: 'Work Colleagues', type: 'item', count: 0 },
+  { title: 'Neighbors / Community Members', type: 'item', count: 0 },
+  { title: 'Parents\' Friends', type: 'item', count: 0 },
+
+  // Others
+  { title: 'Others', type: 'header' },
+  { title: 'Plus Ones / Partners', type: 'item', count: 0 },
+  { title: 'Vendors (Photo/Video)', type: 'item', count: 0 },
+
+  // 2. By Wedding Role
+  { title: 'The Bridal Party', type: 'header' },
+  { title: 'Maid / Matron of Honor', type: 'item', count: 0 },
+  { title: 'Best Man', type: 'item', count: 0 },
+  { title: 'Bridesmaids', type: 'item', count: 0 },
+  { title: 'Groomsmen', type: 'item', count: 0 },
+
+  { title: 'Principal Sponsors (VIPs)', type: 'header' },
+  { title: 'Godparents (Ninong/Ninang)', type: 'item', count: 0 },
+]);
+
+const incrementItem = (item: GuestItem) => {
+  if (item.count !== undefined) item.count++;
+};
+
+const decrementItem = (item: GuestItem) => {
+  if (item.count !== undefined && item.count > 0) item.count--;
+};
+
+const resetCounts = () => {
+  guestList.value.forEach(item => {
+    if (item.count !== undefined) item.count = 0;
+  });
+};
+
+const totalGuestCount = computed(() => {
+  return guestList.value.reduce((sum, item) => sum + (item.count || 0), 0);
 });
 
-const selectedGuestOption = ref("");
+const displayTotalSummary = computed(() => {
+  const count = totalGuestCount.value;
+  if (count === 0) return "";
+  return `${count} Guest${count > 1 ? 's' : ''}`;
+});
 
-// const date_calendar = ref(null);
+// --- END GUEST CALCULATOR LOGIC ---
+
 const dateInput = ref(false);
 const formValid = ref();
-// const eventType = ref(null);
-// const numberOfGuests = ref<number | null>(null);
-// const location = ref<string | null>(null);
-
 const isScrolled = ref(false);
 const offsetTop = ref(0);
 
@@ -261,41 +406,16 @@ const onSubmit = () => {
 };
 
 const searchVenue = () => {
-  // Extract guest count from selectedGuestOption
-  let guestCount = 0;
-  if (selectedGuestOption.value) {
-    // If it's a guest count option like "50 Guests"
-    const guestMatch = selectedGuestOption.value.match(/^(\d+)\s+Guests$/);
-    if (guestMatch) {
-      guestCount = parseInt(guestMatch[1]);
-    }
-    // If it's a range like "10-25 Guests"
-    const rangeMatch = selectedGuestOption.value.match(/^(\d+)-(\d+)\s+Guests$/);
-    if (rangeMatch) {
-      guestCount = parseInt(rangeMatch[1]); // Use lower bound
-    }
-  }
-
   navigateTo({
     name: "country-venues-search",
-    params: { country: location.value.toLowerCase() },
+    params: { country: location.value ? location.value.toLowerCase() : 'default' },
     query: {
       location: location.value,
       date: formattedDateToQuery(),
-      total_guest: guestCount,
-      weddingType: weddingOptions.includes(selectedGuestOption.value) ? selectedGuestOption.value : null,
+      total_guest: totalGuestCount.value, 
     },
   });
 };
-
-const formattedDate = computed(() => {
-  if (!date_calendar.value) return "";
-  const date = new Date(date_calendar.value);
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const day = date.getDate().toString().padStart(2, "0");
-  const year = date.getFullYear();
-  return `${day}/${month}/${year}`;
-});
 
 const formattedCheckInDate = computed(() => {
   if (!checkInDate.value) return "";
@@ -328,11 +448,10 @@ onBeforeMount(async () => {
   await getDefaultCountryImage()
 })
 
-
 function preventNegative(event) {
   if (event.key === '-' || event.key === 'e') {
-      event.preventDefault();
-    }
+    event.preventDefault();
+  }
 }
 </script>
 
